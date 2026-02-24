770baeb736f6826034a76ff006782f1e71f9c478
Gustavo <gustavobispocosta5521@gmail.com>
2026-02-24 02:12:14 -0300

    Crud em Java com SpringBoot

---
+++ src/test/java/com/GuGuzin14/cadastro_usuario/CadastroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GuGuzin14.cadastro_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
